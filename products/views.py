aadae54e2a6f747b21b0fdda5f76e1a8f9a133dc
OBY <jphalisnj@gmail.com>
2015-09-13 13:28:59 -0400

    newest changes

--- products/views.py
+++ products/views.py
@@ -4,40 +4,46 @@ from .models import Product, ProductImage
 
 # Create your views here.
 
+
+def massage(request):
+    return render(request, 'massage.html', {})
+
+
 def products(request):
-	products = Product.objects.all()
-	context = {'products': products}
-	template = 'products/products.html'
-	return render(request, template, context)
+    products = Product.objects.all()
+    context = {'products': products}
+    template = 'products/products.html'
+    return render(request, template, context)
+
 
 def search(request):
-	try:
-		q = request.GET.get('q')
-	except:
-		q = None
+    try:
+        q = request.GET.get('q')
+    except:
+        q = None
 
-	if q:
-		products = Product.objects.filter(title__icontains=q)
-		context = {
-			'query': q, 
-			'products': products
-		}
-		template = 'products/results.html'
-	else:
-		context = {}
-		template = 'products/products.html'
-	return render(request, template, context)
+    if q:
+        products = Product.objects.filter(title__icontains=q)
+        context = {
+            'query': q,
+            'products': products
+        }
+        template = 'products/results.html'
+    else:
+        context = {}
+        template = 'products/products.html'
+    return render(request, template, context)
+
 
 def single(request, slug):
-	try:
-		product = Product.objects.get(slug=slug)
-		images = ProductImage.objects.filter(product=product)
-		context = {
-			'product': product,
-			'images': images
-		}
-		template = 'products/single.html'
-		return render(request, template, context)
-	except:
-		raise Http404
-		
+    try:
+        product = Product.objects.get(slug=slug)
+        images = ProductImage.objects.filter(product=product)
+        context = {
+            'product': product,
+            'images': images
+        }
+        template = 'products/single.html'
+        return render(request, template, context)
+    except:
+        raise Http404
